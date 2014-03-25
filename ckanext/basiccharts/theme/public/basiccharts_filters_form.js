a1654109f1739d691ff884894b49abf21dbc8102
Vitor Baptista <vitor@vitorbaptista.com>
2014-03-25 12:50:14 -0300

    Refactoring basiccharts_filters_form.js

--- ckanext/basiccharts/theme/public/basiccharts_filters_form.js
+++ ckanext/basiccharts/theme/public/basiccharts_filters_form.js
@@ -2,20 +2,19 @@ ckan.module("basiccharts_filters_form", function (jQuery) {
   "use strict";
 
   function applyDropdown(selectField, filterValues) {
-    var self = this,
-        inputField = selectField.parent().find('input'),
-        input_value = inputField.val(),
+    var inputField = selectField.parent().find('input'),
+        inputValue = inputField.val(),
         valueList;
 
     valueList = filterValues[selectField.val()];
     if (valueList) {
       inputField.replaceWith('<input type="hidden" value="" name="filter_values">');
-      inputField = selectField.parent().find('input'),
-      inputField.val(input_value);
+      inputField = selectField.parent().find('input');
+      inputField.val(inputValue);
       inputField.select2({
           data: valueList,
           width: '220px'
-      })
+      });
     }
   }
 
@@ -25,35 +24,35 @@ ckan.module("basiccharts_filters_form", function (jQuery) {
         filtersDiv = self.el.find(self.options.filtersSelector),
         addFilterEl = self.el.find(self.options.addFilterSelector),
         removeFilterSelector = self.options.removeFilterSelector,
-        filterValues = self.options.filterValues;
+        filterValues = self.options.filterValues || {};
 
     var selects = filtersDiv.find('select');
     selects.each(function (i, select) {
        applyDropdown($(select), filterValues);
-    })
+    });
 
     addFilterEl.click(function (evt) {
-      var inputField, selectField, valueList, input_value
+      var selectField;
       evt.preventDefault();
       filtersDiv.append(templateFilterInputs);
       selectField = filtersDiv.children().last().find('select');
-      applyDropdown(selectField, filterValues)
+      applyDropdown(selectField, filterValues);
     });
 
-
-    filtersDiv.on("click", removeFilterSelector, function (evt) {
+    filtersDiv.on('click', removeFilterSelector, function (evt) {
       evt.preventDefault();
       $(this).parent().remove();
     });
 
-    filtersDiv.on("change", 'select', function (evt) {
-      var inputField = $(this).parent().find('input');
-      var select2Contianer = $(this).parent().find('.select2-container');
+    filtersDiv.on('change', 'select', function (evt) {
+      var el = $(this),
+          parentEl = el.parent(),
+          inputField = parentEl.find('input'),
+          select2Container = parentEl.find('.select2-container');
       evt.preventDefault();
-      select2Contianer.remove();
+      select2Container.remove();
       inputField.replaceWith('<input type="text" value="" name="filter_values">');
-      inputField = $(this).parent().find('input');
-      applyDropdown($(this), filterValues);
+      applyDropdown(el, filterValues);
     });
   }
 
